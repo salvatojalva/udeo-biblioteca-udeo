448ff15e6f34c8b74566c982b9239b840d406021
Glenda <gr.cabrera@udeo.edu.gt>
2021-09-28 22:02:18 -0600

    paginacion controlador usuarios

--- BibliotecaUDEO/Program.cs
+++ BibliotecaUDEO/Program.cs
@@ -18,14 +18,17 @@ namespace BibliotecaUDEO
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((context, config) =>
+
+            Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
             {
-                var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-                config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+                //var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+                //config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
             })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
             });
+
     }
 }
